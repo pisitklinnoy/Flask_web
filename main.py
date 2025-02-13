70d83b47efb1df53c61c562a712bf9cb8da6a05b
pisitklinnoy <tcmobile0212602@gmail.com>
2025-02-13 18:17:07 +0700

    add main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import flask
+import models
+import forms
+import acl
+
+from flask_login import login_required, login_user, logout_user
+from flask import Response, send_file, abort
+
+app = flask.Flask(__name__)
+app.config["SECRET_KEY"] = "This is secret key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+models.init_app(app)
+
+
+@app.route("/")
+def index():
+    db = models.db
+    notes = db.session.execute(
+        db.select(models.Note).order_by(models.Note.title)
+    ).scalars()
+    return flask.render_template(
+        "index.html",
+        notes=notes,
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
